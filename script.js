a3f81833c4902dc36883b1b9f1a574d5aefcd932
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-11 21:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-// Constructor function for Car
 function Car(make, model) {
   this.make = make;
   this.model = model;
 }
-
-// Method added to Car prototype
 Car.prototype.getMakeModel = function() {
   return `${this.make} ${this.model}`;
 };
-
-// Constructor function for SportsCar, inheriting from Car
 function SportsCar(make, model, topSpeed) {
-  // Call Car constructor with make and model
   Car.call(this, make, model);
   this.topSpeed = topSpeed;
 }
-
-// Inherit Car prototype methods using Object.create
 SportsCar.prototype = Object.create(Car.prototype);
-
-// Set SportsCar constructor to itself
 SportsCar.prototype.constructor = SportsCar;
-
-// Method added to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function() {
   return this.topSpeed;
 };
-
-// Example usage:
 let myCar = new Car("Toyota", "Camry");
-console.log(myCar.getMakeModel()); // Output: "Toyota Camry"
+console.log(myCar.getMakeModel());
 
 let mySportsCar = new SportsCar("Ferrari", "488 GTB", 330);
-console.log(mySportsCar.getMakeModel()); // Output: "Ferrari 488 GTB"
-console.log(mySportsCar.getTopSpeed()); // Output: 330
+console.log(mySportsCar.getMakeModel()); 
+console.log(mySportsCar.getTopSpeed()); 
 
